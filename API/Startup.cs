e04455456274bbfa111b78e46e438050b59d586d
webtree <web-tree@yandex.ru>
2021-09-23 21:57:25 +0400

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,12 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +58,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
