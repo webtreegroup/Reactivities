24ea3e1f2149f1c1c969c810a6f73d46a4cd5b03
webtree <web-tree@yandex.ru>
2021-10-17 16:15:13 +0400

    config pre-commit

--- client-app/src/core/api/api.activity.ts
+++ client-app/src/core/api/api.activity.ts
@@ -4,5 +4,5 @@ import { requests } from './api'
 const BASE_URL = '/activities'
 
 export const ActivityApi = {
-    list: () => requests.get<ActivityProps[]>(BASE_URL)
+	list: () => requests.get<ActivityProps[]>(BASE_URL),
 }
--- client-app/src/core/api/api.ts
+++ client-app/src/core/api/api.ts
@@ -6,8 +6,9 @@ axios.defaults.baseURL = API_URL
 const responseBody = <T>(response: AxiosResponse<T>) => response.data
 
 export const requests = {
-    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-    post: <T>(url: string, body: T) => axios.post<T>(url, body).then(responseBody),
-    put: <T>(url: string, body: T) => axios.put<T>(url, body).then(responseBody),
-    delete: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+	get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+	post: <T>(url: string, body: T) =>
+		axios.post<T>(url, body).then(responseBody),
+	put: <T>(url: string, body: T) => axios.put<T>(url, body).then(responseBody),
+	delete: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 }
